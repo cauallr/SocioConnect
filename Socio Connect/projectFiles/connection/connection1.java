8ad5ce46f4d59f4a033b9142dd6f9fbd087532ad
cauallr <132109162+cauallr@users.noreply.github.com>
2023-09-02 19:52:33 -0300

    mais uma tentativa banco de dados

--- Socio Connect/projectFiles/connection/connection1.java
+++ Socio Connect/projectFiles/connection/connection1.java
@@ -1,15 +1,26 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class connection1 {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Connection conexao = null;
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Carregue a classe do driver JDBC correta
+            Class.forName("com.mysql.jdbc.Driver");
+
+            // Estabeleça a conexão com o MySQL, substituindo "root" e "sua_senha" pelos seus valores
             conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/socio_connect?user=root&password=");
-            
+     
+
+            // Execute uma consulta SQL, substituindo pela sua consulta desejada
+            ResultSet rsusuarios = conexao.createStatement().executeQuery("SELECT * FROM USUARIOS");
+
             System.out.println("Conexão estabelecida com sucesso!");
 
             // Aqui você pode executar operações no banco de dados
